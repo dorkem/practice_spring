08a5c2d7d3327889c8028ba033fc5e859b20be6d
dorkem <20233144@gs.cwnu.ac.kr>
2025-04-30 20:59:15 +0900

    :sparkles: 수정, 삭제 추가함

--- src/test/java/hello/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/repository/MemberRepositoryV0Test.java
@@ -4,6 +4,7 @@ import hello.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
@@ -13,7 +14,7 @@ class MemberRepositoryV0Test {
 
     @Test
     void curd() throws SQLException {
-        Member member = new Member("memberV2", 10000);
+        Member member = new Member("memberV100", 10000);
 
         //save
         repository.save(member);
@@ -22,5 +23,15 @@ class MemberRepositoryV0Test {
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember = {}", findMember);
         assertThat(findMember).isEqualTo(member);
+
+        //update
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
